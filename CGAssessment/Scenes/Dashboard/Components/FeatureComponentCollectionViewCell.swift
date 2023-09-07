32bb3e705299e3a0aa986a69ae008fc73cd37226
Diego Henrique Silva Oliveira <diegohenrique.so2000@gmail.com>
2023-09-06 22:18:26 -0300

    TK-1: Creating feature component, adding needed fonts and colors

---
+++ CGAssessment/Scenes/Dashboard/Components/FeatureComponentCollectionViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeatureComponentCollectionViewCell.swift
+//  CGAssessment
+//
+//  Created by Diego Henrique Silva Oliveira on 06/09/23.
+//
+
+import UIKit
+
+class FeatureComponentCollectionViewCell: UICollectionViewCell {
+
+    // MARK: Private Properties
+    
+    @IBOutlet private weak var titleLabel: UILabel?
+    @IBOutlet private weak var iconLabel: UILabel?
+    
+    // MARK: Life Cycle
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
