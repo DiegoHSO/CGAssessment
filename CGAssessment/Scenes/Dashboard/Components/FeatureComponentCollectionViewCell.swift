ad95121a59ff4964a20db0508fb1a6ca59e72719
Diego Henrique Silva Oliveira <diegohenrique.so2000@gmail.com>
2023-09-07 00:19:00 -0300

    TK-1: Creating setup method for component

--- CGAssessment/Scenes/Dashboard/Components/FeatureComponentCollectionViewCell.swift
+++ CGAssessment/Scenes/Dashboard/Components/FeatureComponentCollectionViewCell.swift
@@ -9,16 +9,22 @@ import UIKit
 
 class FeatureComponentCollectionViewCell: UICollectionViewCell {
 
-    // MARK: Private Properties
+    // MARK: - Private Properties
     
     @IBOutlet private weak var titleLabel: UILabel?
     @IBOutlet private weak var iconLabel: UILabel?
     
-    // MARK: Life Cycle
+    // MARK: - Life Cycle
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    // MARK: - Public Methods
+    
+    func setup(title: String, iconSymbol: String) {
+        titleLabel?.text = title
+        iconLabel?.text = iconSymbol
+    }
 }
