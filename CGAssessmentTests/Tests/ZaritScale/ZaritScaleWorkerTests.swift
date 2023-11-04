14d1233fad1fa25af92c476769706710d0921aaf
Diego Henrique Silva Oliveira <diegohenrique.so2000@gmail.com>
2023-11-04 00:15:15 -0300

    TK-67: Adding ZaritScale unit tests

---
+++ CGAssessmentTests/Tests/ZaritScale/ZaritScaleWorkerTests.swift
@@ -0,0 +1,102 @@
+//
+//  ZaritScaleWorkerTests.swift
+//  CGAssessmentTests
+//
+//  Created by Diego Henrique Silva Oliveira on 04/11/23.
+//
+
+import CoreData
+import XCTest
+@testable import CGAssessment
+
+final class ZaritScaleWorkerTests: XCTestCase {
+
+    // MARK: - Private Properties
+
+    private var currentExpectation: XCTestExpectation?
+    private var dao: CoreDataDAOMock?
+
+    // MARK: - Life Cycle
+
+    override func setUp() {
+        super.setUp()
+        dao = CoreDataDAOMock()
+        try? dao?.addStandaloneCGA()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        currentExpectation = nil
+        dao = nil
+    }
+
+    // MARK: - Test Methods
+
+    func testGetAssessmentProgress() {
+        let newExpectation = expectation(description: "Call getAssessmentProgress")
+        currentExpectation = newExpectation
+
+        guard let cgaId = UUID(uuidString: "0734772b-cd5b-4392-9148-7bf1994dd8d3") else {
+            XCTFail("Unexpected UUID")
+            return
+        }
+
+        let worker = ZaritScaleWorker(dao: dao ?? DAOFactory.coreDataDAO, cgaId: cgaId)
+
+        do {
+            let test = try worker.getZaritScaleProgress()
+
+            guard let questionOptions = test?.selectableOptions?.allObjects as? [SelectableOption] else {
+                XCTFail("Unable to get selectable options for test")
+                return
+            }
+
+            var rawQuestions: ZaritScaleModels.RawQuestions = [:]
+
+            questionOptions.forEach { option in
+                guard let selectedOption = SelectableKeys(rawValue: option.selectedOption),
+                      let identifier = LocalizedTable(rawValue: option.identifier ?? "") else { return }
+                rawQuestions[identifier] = selectedOption
+            }
+
+            XCTAssertEqual(rawQuestions, [.zaritScaleQuestionOne: .firstOption, .zaritScaleQuestionTwo: .firstOption, .zaritScaleQuestionThree: .firstOption,
+                                          .zaritScaleQuestionFour: .firstOption, .zaritScaleQuestionFive: .secondOption, .zaritScaleQuestionSix: .secondOption,
+                                          .zaritScaleQuestionSeven: .secondOption
+            ])
+            XCTAssertEqual(test?.isDone, true)
+            currentExpectation?.fulfill()
+        } catch {
+            XCTFail("Test failed with error \(error.localizedDescription)")
+        }
+
+        wait(for: [newExpectation], timeout: 1)
+    }
+
+    func testSaveAssessmentProgress() {
+        let saveExpectation = expectation(forNotification: .NSManagedObjectContextDidSave, object: dao?.context) { _ in
+            return true
+        }
+
+        let newExpectation = expectation(description: "Call saveAssessmentProgress")
+        currentExpectation = newExpectation
+
+        guard let cgaId = UUID(uuidString: "0734772b-cd5b-4392-9148-7bf1994dd8d3") else {
+            XCTFail("Unexpected UUID")
+            return
+        }
+
+        let worker = ZaritScaleWorker(dao: dao ?? DAOFactory.coreDataDAO, cgaId: cgaId)
+
+        do {
+            try worker.updateZaritScaleProgress(with: .init(questions: [.zaritScaleQuestionOne: .thirdOption, .zaritScaleQuestionTwo: .secondOption, .zaritScaleQuestionThree: .secondOption,
+                                                                        .zaritScaleQuestionFour: .firstOption, .zaritScaleQuestionFive: .thirdOption, .zaritScaleQuestionSix: .secondOption,
+                                                                        .zaritScaleQuestionSeven: .firstOption
+            ], isDone: false))
+            currentExpectation?.fulfill()
+        } catch {
+            XCTFail("Test failed with error \(error.localizedDescription)")
+        }
+
+        wait(for: [saveExpectation, newExpectation], timeout: 1)
+    }
+}
